f7723be4ea7a6fb8b28d9136c32f4f75355bf02c
Raphael Dichler <raphael@dichler.com>
2020-12-15 20:22:16 +0100

    Initial commit

---
+++ src/main/java/net/plyse/chess/Main.java
@@ -0,0 +1,13 @@
+package net.plyse.chess;
+
+/**
+ * @author Raphael Dichler on 15.12.20
+ * @project Chess
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
